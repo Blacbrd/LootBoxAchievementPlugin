6137637231943073f633e202d60814c05fdf06eb
Blacbrd <blacbrd123@gmail.com>
2024-03-30 17:06:56 +0100

    Just a few classes made

---
+++ src/main/java/me/blacbrd123/lootboxachievement/LootboxAchievement.java
@@ -0,0 +1,28 @@
+package me.blacbrd123.lootboxachievement;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LootboxAchievement extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+        loadCommands();
+        loadListeners();
+
+    }
+
+    public void loadCommands(){
+
+    }
+
+    public void loadListeners(){
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/me/blacbrd123/lootboxachievement/listeners/PlayerAchievementListener.java
@@ -0,0 +1,14 @@
+package me.blacbrd123.lootboxachievement.listeners;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+
+public class PlayerAchievementListener implements Listener {
+
+    public void onAchievement(PlayerAdvancementDoneEvent event){
+
+
+
+    }
+
+}
